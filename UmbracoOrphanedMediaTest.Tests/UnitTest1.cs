374882fec48cd35204a8fd5b565845083208a2cb
Robert Salmon <robdavidsalmon@msn.com>
2022-03-18 10:58:32 +0000

    Add project files.

---
+++ UmbracoOrphanedMediaTest.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UmbracoOrphanedMediaTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
